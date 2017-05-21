bd677b88df41ad37d095a5b421f83c33cb0d441c
beepscore <support@beepscore.com>
2017-05-21 12:28:13 -0700

    In Swift framework project add PathFinder.swift

---
+++ PathFinder/PathFinder.swift
@@ -0,0 +1,13 @@
+//
+//  PathFinder.swift
+//  PathFinder
+//
+//  Created by Steve Baker on 5/21/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class PathFinder: NSObject {
+
+}

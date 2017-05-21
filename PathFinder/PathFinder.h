e3ebddaedc6cdcd898ba79b9fb48ac798d6aced2
beepscore <support@beepscore.com>
2017-05-21 12:26:12 -0700

    Initial Commit

---
+++ PathFinder/PathFinder.h
@@ -0,0 +1,19 @@
+//
+//  PathFinder.h
+//  PathFinder
+//
+//  Created by Steve Baker on 5/21/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PathFinder.
+FOUNDATION_EXPORT double PathFinderVersionNumber;
+
+//! Project version string for PathFinder.
+FOUNDATION_EXPORT const unsigned char PathFinderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathFinder/PublicHeader.h>
+
+

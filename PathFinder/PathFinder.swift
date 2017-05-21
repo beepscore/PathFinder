fdc5d69dcb3a86ad15ff53b0bab1593036ed38c0
beepscore <support@beepscore.com>
2017-05-21 12:35:38 -0700

    In PathFinder add numberOfUniquePaths. Add tests. TODO: Clean up code.

--- PathFinder/PathFinder.swift
+++ PathFinder/PathFinder.swift
@@ -6,8 +6,59 @@
 //  Copyright © 2017 Beepscore LLC. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class PathFinder: NSObject {
 
+/*
+
+ A Turtle is placed on the top left corner of a m by n grid. The turtle can only move right or move down by one cell at a time. Implement the method that computes the total number of unique paths that the turtle can take to reach the bottom right corner.
+
+ */
+
+    // entire board is clear, no obstacles
+    
+
+// m + n - 2 moves
+// m - 1 moves horizontally, n -1 moves vertically
+// C(m-1, m+n-2) = (m+n-2) * (m+n-3) * (m+n-4).... * ((m+n-2) - (m-1) +1) / (m-1)!
+
+//  struct Position(row:Int, column:Int) {
+//    self.row = row
+//    self.column = column
+//  }
+
+    class func numberOfUniquePaths(rows: Int, columns: Int) -> Int {
+
+        // first move has 2 choices- right or down
+        // each path takes m-1 steps left plus n-1 steps down
+
+        // could represent each path as a (m-1)+(n-1) digit binary number
+        // where left is 0 and down is 1
+        // uniquePaths = []
+        // 10
+
+        // base case
+        if (rows == 1 && columns == 1) {
+            // we got to bottom corner, finished a path
+            return 1
+        }
+
+        if rows < 2 {
+            // can't move down, move right
+            return numberOfUniquePaths(rows: rows, columns: columns - 1)
+        }
+
+        // moveRightIfPossible
+        if columns < 2 {
+            // can't move right, move down
+            return numberOfUniquePaths(rows: rows - 1, columns: columns)
+        }
+
+        return numberOfUniquePaths(rows: rows - 1, columns: columns)
+            + numberOfUniquePaths(rows: rows, columns: columns - 1)
+    }
+
+
 }
+
--- PathFinderTests/PathFinderTests.swift
+++ PathFinderTests/PathFinderTests.swift
@@ -10,27 +10,23 @@ import XCTest
 @testable import PathFinder
 
 class PathFinderTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testNumberOfUniquePaths() {
+
+        var rows = 2
+        var columns = 2
+        var numberOfUniquePaths = PathFinder.numberOfUniquePaths(rows:rows, columns:columns)
+        XCTAssertEqual(numberOfUniquePaths, 2)
+
+        rows = 2
+        columns = 3
+        numberOfUniquePaths = PathFinder.numberOfUniquePaths(rows:rows, columns:columns)
+        XCTAssertEqual(numberOfUniquePaths, 3)
+
+        rows = 3
+        columns = 3
+        numberOfUniquePaths = PathFinder.numberOfUniquePaths(rows:rows, columns:columns)
+        XCTAssertEqual(numberOfUniquePaths, 6)
     }
     
 }
